88562b5b1d1fb0be280cc9a0429cd5463d8c0b3c
voduchuy2001 <voduchuy2001@gmail.com>
2023-08-12 16:35:10 +0700

    make activity logs

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,12 +9,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Rappasoft\LaravelAuthenticationLog\Traits\AuthenticationLoggable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
     use Notifiable;
+    use AuthenticationLoggable;
 
     protected $fillable = [
         'name',
