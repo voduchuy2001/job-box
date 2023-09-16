49ec00e95cf06e186f5595e2b13ca5014767d37d
voduchuy2001 <voduchuy2001@gmail.com>
2023-09-17 00:06:00 +0700

    update

--- resources/views/livewire/user/job/wishlist.blade.php
+++ resources/views/livewire/user/job/wishlist.blade.php
@@ -3,5 +3,6 @@
         wire:click="addOrRemoveToWishList"
         type="button"
         class="btn btn-icon btn-soft-{{ in_array($jobId, $wishlist) ? 'danger' : 'primary' }} float-end {{ in_array($jobId, $wishlist) ? 'active' : '' }}">
-        <i class="{{ in_array($jobId, $wishlist) ? 'mdi mdi-cards-heart' : 'ri-bookmark-line' }} fs-16"></i></x-button>
+        <span wire:loading class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+        <i wire:loading.class="d-none" class="{{ in_array($jobId, $wishlist) ? 'mdi mdi-cards-heart' : 'ri-bookmark-line' }} fs-16"></i></x-button>
 </div>
