384660b2f9a65c2c8b2cca33365343dbefebf1fc
voduchuy2001 <voduchuy2001@gmail.com>
2023-09-16 23:55:37 +0700

    update

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -17,8 +17,6 @@ class Address extends Model
         'ward_id',
         'district_id',
         'province_id',
-        'longitude',
-        'latitude',
     ];
 
     public function addressable(): MorphTo
