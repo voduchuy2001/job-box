fd389cfb39393fd4d750c68800fb340346c10833
voduchuy2001 <voduchuy2001@gmail.com>
2023-09-17 17:06:30 +0700

    chores

--- resources/views/livewire/user/job/wishlist.blade.php
+++ resources/views/livewire/user/job/wishlist.blade.php
@@ -2,6 +2,6 @@
     <x-button
         wire:click="addOrRemoveToWishList"
         type="button"
-        class="btn btn-icon btn-soft-primary float-end {{ in_array($jobId, $wishlist) ? 'active' : '' }}">
+        class="btn btn-icon btn-soft-info float-end {{ in_array($jobId, $wishlist) ? 'active' : '' }}">
         <i class="ri-bookmark-line fs-16"></i></x-button>
 </div>
