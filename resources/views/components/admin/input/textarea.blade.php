54c2656f3dece70b80fb1cd5ef45a519ca22a0c5
voduchuy2001 <voduchuy2001@gmail.com>
2023-09-22 09:01:19 +0700

    chores

--- resources/views/components/admin/input/textarea.blade.php
+++ resources/views/components/admin/input/textarea.blade.php
@@ -13,7 +13,7 @@
     @if($label) <label for="{{ $id }}" class="form-label">{{ $label }}</label> @endif
     <textarea
         @if($type) type="{{ $type }}" @endif
-        @if($class) class="{{ $class }}" @endif
+        @if($class) class="{{ $class }} @error($model) is-invalid @enderror" @endif
         @if($id) id="{{ $id }}" @endif
         @if($placeholder) placeholder="{{ $placeholder }}" @endif
         @if($model) wire:model="{{ $model }}" @endif
